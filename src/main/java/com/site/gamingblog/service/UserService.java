cc7d57cc05a0bc9b54c409b91adc5de96164355c
KrystianKaz <krystian1991@o2.pl>
2022-12-26 14:48:59 +0100

    userservice autowired repo

--- src/main/java/com/site/gamingblog/service/UserService.java
+++ src/main/java/com/site/gamingblog/service/UserService.java
@@ -4,6 +4,7 @@ import com.site.gamingblog.exception.*;
 import com.site.gamingblog.model.User;
 import com.site.gamingblog.model.enums.UserRolesEnum;
 import com.site.gamingblog.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +17,7 @@ import java.util.Optional;
 public class UserService {
 
 
+    @Autowired
     private final UserRepository userRepository;
 
     public UserService(final UserRepository userRepository) {
@@ -33,7 +35,7 @@ public class UserService {
     }
 
     public void addUser(String email, String username, String password) {
-        if (!checkIfUserWithGivenNicknameIsInDatabase(username)) ;
+        if (!checkIfUserWithGivenNicknameIsInDatabase(username))
         {
             BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
             User user = User.builder()
